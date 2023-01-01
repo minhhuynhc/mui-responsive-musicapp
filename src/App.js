8d0f7e470001e78ab8695719e0764db5c91de2e7
convitcs <94104858+minhhuynhc@users.noreply.github.com>
2023-01-01 17:17:30 +0700

    fetch user playlist from spotify

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import { createTheme } from "@mui/material";
+import { Button, Chip, createTheme, Menu, MenuItem } from "@mui/material";
 import { getTokenFromResponse } from "./userData/getToken";
 import Router from "./Routes";
 import ThemeProvider from "./theme";
 import SpotifyWebApi from "spotify-web-api-js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStateValue } from "./userData/StateProvider";
+import { useCookies } from "react-cookie";
 
 const s = new SpotifyWebApi();
 
 function App() {
-  const [{ token, discover_weekly }, dispatch] = useStateValue();
+  const [{ token, user }, dispatch] = useStateValue();
+  const [cookie, setCookie, removeCookie] = useCookies(['token'])
 
+  // sessionStorage.setItem('token', null)
+  
   useEffect(() => {
     const hash = getTokenFromResponse();
     window.location.hash = "";
     let _token = hash.access_token;
-
+    console.log({_token})
     if (_token) {
-      s.setAccessToken(_token);
+      sessionStorage.setItem('token', _token)}
 
+    if (sessionStorage.getItem('token')){
+      s.setAccessToken(sessionStorage.getItem('token'));
+      s.getMe().then((user) => {
+        console.log('khong co loi')
+        dispatch({
+
+          type: "SET_USER",
+          user,
+        });
+      })
+      .catch((err) => {console.log(err)});
+      user && s.getUserPlaylists(user.id).then((playlists)=>{
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+        console.log(playlists)
+      })
+      .catch((err) => {console.log(err)});
+      }
+      s.getPlaylist("37i9dQZF1DX9XIFQuFvzM4").then((response) =>{
       dispatch({
-        type: "SET_TOKEN",
-        token: _token,
-      });
-    }
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: response,
+      })
+      console.log({response})}
+      );
+      console.log('token in session:'+sessionStorage.getItem('token'))
   }, [token, dispatch])
-  console.log()
+  // console.log(!Boolean(sessionStorage.getItem('token')==='null'))
+  // console.log({user})
+ 
   return (
     <ThemeProvider>
       <Router/>
---
+++ src/layouts/dashboard/nav/Playlists.js
@@ -0,0 +1,28 @@
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import React from 'react';
+import { useStateValue } from '../../../userData/StateProvider';
+
+const Playlists = () => {
+  const [{playlists}, dispatch] = useStateValue()
+  return (
+    <List sx={{}}>
+      {console.log({playlists})}
+        {playlists.items.map((playlist) => (
+          <Playlist title={playlist.name} />
+        ))}
+      </List>
+  );
+};
+
+export default Playlists;
+
+
+const Playlist = ({ title }) => {
+    return (
+      <ListItem disablePadding>
+        <ListItemButton sx={{ color: "greys" }}>
+          <ListItemText primary={title} sx={{ color: "gray" }} />
+        </ListItemButton>
+      </ListItem>
+    );
+  };
